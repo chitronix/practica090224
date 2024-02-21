11a1ebf8b1acfa5490cc5120a32efa09bda296b7
christofer <chitronix@LAPTOP-023TN83H>
2024-02-20 19:50:29 -0600

    Ejemplo de Usuario y video de java primera clase para el profe alfonso

--- src/main/java/com/mycompany/clasehoy/COMENTARIO.java
+++ src/main/java/com/mycompany/clasehoy/COMENTARIO.java
@@ -1,14 +1,4 @@
 package com.mycompany.clasehoy;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author repty
- */
 public class COMENTARIO {
      String Comentario;
     usuario Autor;
